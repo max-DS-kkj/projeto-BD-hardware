8e150eb4b78f3e0ee08e9aa435384fa90937a954
max-DS-kkj <l.max@escola.pr.gov.br>
2026-09-18 08:06:47 -0300

    tabela categoria

--- script-bd-hardware.sql
+++ script-bd-hardware.sql
@@ -6,3 +6,10 @@ CREATE TABLE pagamento(
   status_pagamento varchar(50),
   id_compra bigint NOT NULL
 );
+
+CREATE TABLE categoria(
+  id_categoria SERIAL PRIMARY KEY,
+  nome varchar(100) NOT NULL,
+  descricao text
+);
+
